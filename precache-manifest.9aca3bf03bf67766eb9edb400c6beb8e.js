d90975a807cf34da6c3019a1729e15bdd927e03a
Jason Kim <jasonkim4201@gmail.com>
2019-03-07 09:29:04 -0500

    Updates

---
+++ precache-manifest.9aca3bf03bf67766eb9edb400c6beb8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7402ba7e4dfd5d84a7694bd128c48d99",
+    "url": "/clicky-game/static/media/beerCardHeader.7402ba7e.png"
+  },
+  {
+    "revision": "948775b8234aed8d4d5657ca262bf670",
+    "url": "/clicky-game/static/media/alcohol-alcoholism-ale-background-bar-beer-1457325-pxhere.com.948775b8.jpg"
+  },
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.7985e359.js"
+  },
+  {
+    "revision": "2bda9b5fe6599d87f19a",
+    "url": "/clicky-game/static/js/main.2bda9b5f.chunk.js"
+  },
+  {
+    "revision": "6320b01e35af4e1afba2",
+    "url": "/clicky-game/static/js/2.6320b01e.chunk.js"
+  },
+  {
+    "revision": "2bda9b5fe6599d87f19a",
+    "url": "/clicky-game/static/css/main.f5925f78.chunk.css"
+  },
+  {
+    "revision": "cfc56f93f4a7ea97209a99f0df0e06eb",
+    "url": "/clicky-game/index.html"
+  }
+];
